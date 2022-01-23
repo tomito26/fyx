ba7622eef92b1254b4395512ddd80599bf7380a6
tomito26 <tommybwah@gmail.com>
2022-01-23 23:42:18 +0300

    created a new project

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function initMap(){
+    const options = {
+        center:{lat:-1.286389,lng:36.817223},
+        zoom:14
+    };
+
+    map = new  google.maps.Map(document.querySelector(".map"),options);
+}
