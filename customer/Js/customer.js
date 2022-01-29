7b1dbb44bdebe4e3f33fd1abc7d34e1a2a3af308
tomito26 <tommybwah@gmail.com>
2022-01-30 01:02:47 +0300

    added customer app where users can book service,give feedback and make payments

---
+++ customer/Js/customer.js
@@ -0,0 +1,8 @@
+
+const navLinks = document.querySelectorAll("nav li a");
+const iframe = document.querySelector(".iframe")
+const activeOrder = 
+
+console.log(iframe.src)
+
+
--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
+function User(fullname,email,password){
+    this.fullname = fullname;
+    this.email = email;
+    this.password=password;
+}
+
+let users = []
+
+
+const form = document.querySelector(".createAccountForm");
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const userPassword = document.querySelector("#password").value;
+    const confirmPassword = document.querySelector("#confirmpassword").value;
+    
+    console.log(name)
+    if(name !== "",email !== "",userPassword !== "",confirmPassword !== ""){
+        let user = new User(name,email,userPassword);
+        users.push(user)
+        window.location.href="../../customer/customer.html"
+    }
+    console.log(users)
+    
+})
+
+
 function initMap(){
     const options = {
         center:{lat:-1.286389,lng:36.817223},
@@ -6,3 +35,5 @@ function initMap(){
 
     map = new  google.maps.Map(document.querySelector(".map"),options);
 }
+// console.log("tom")
+
