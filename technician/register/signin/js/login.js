799b120d8896a6e24ff23d5abfd74bf463c4bbf3
tomito26 <tommybwah@gmail.com>
2022-02-12 23:24:27 +0300

    added  conditional statement that ensures that the input is not empty and the user enters the correct password in both the login and register.js

--- technician/register/signin/js/login.js
+++ technician/register/signin/js/login.js
@@ -5,16 +5,25 @@ function getLoginDetails(){
 getLoginDetails()
 
 let form = document.querySelector("#customerLogin");
+const warning = document.querySelector(".warning-text")
+// console.log(warning)
 form.addEventListener("submit",(e)=>{
     e.preventDefault();
     const email = document.querySelector(".userEmail").value;
     const password = document.querySelector(".userPassword").value;
 
-    const user = sessionStorage.getItem("user");
+    const user = localStorage.getItem("user");
     const technician = JSON.parse(user)
     console.log(technician)
-    if(email === technician.email && password === technician.password){
+    if(email !== "" && password !== ""){
+        if(email === technician.email && password === technician.password){
         location.href = "/technician/technician.html"
+        //  console.log("we have logged in")
+        }else{
+            warning.innerText = "You have entered  incorrect email or password"
+        }
+    }else{
+        warning.innerText = "Please fill the details to login"
     }
     
     
--- technician/register/singup/js/register.js
+++ technician/register/singup/js/register.js
@@ -7,7 +7,10 @@ function User(fullname,email,password){
 let users = []
 
 
+
 const form = document.querySelector(".createAccountForm");
+const warning =  document.querySelector(".warning-text");
+console.log(warning)
 
 form.addEventListener("submit",(e)=>{
     e.preventDefault()
@@ -18,11 +21,21 @@ form.addEventListener("submit",(e)=>{
     
     console.log(name)
     if(name !== "" && email !== "" && userPassword !== "" && confirmPassword !== ""){
-        let user = new User(name,email,userPassword);
-        users.push(user)
-        sessionStorage.setItem("user",JSON.stringify(user))
-        location.href="/technician/register/signin/login.html"
+        if (userPassword === confirmPassword){
+            let user = new User(name,email,userPassword);
+            users.push(user)
+            localStorage.setItem("user",JSON.stringify(user))
+            console.log(localStorage)
+            location.href="/technician/register/signin/login.html"
+        }else{
+            warning.innerText = "The password entered doesn't match confirm password"
+        }
+  
+    }else{
+        warning.innerText = "Please fill the details"
     }
     console.log(users)
     
 })
+
+// const setItem = ()
